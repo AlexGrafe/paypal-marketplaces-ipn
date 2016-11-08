d806a7c69137508443ae50a27911567ec80c59ce
Alex Grafe <alex.grafe@xanderia.org>
2016-11-08 15:57:31 +0100

    + POST

--- server.js
+++ server.js
@@ -1,13 +1,27 @@
 var express = require('express');
 var app = express();
 
+
+
 app.get('/', function (req, res) {
    res.send('Hello World');
+});
+
+
+app.post('*', function (req, res) {
+   console.log("Got a POST request for the homepage", req);
+   res.send(JSON.stringify(req));
 })
+
+
+
+
+
+
 
 var server = app.listen(process.env.PORT || 8080, function () {
    var host = server.address().address
    var port = server.address().port
 
-   console.log("Example app listening at http://%s:%s", host, port)
-})
+   console.log("Example app listening at ", host, port)
+});
