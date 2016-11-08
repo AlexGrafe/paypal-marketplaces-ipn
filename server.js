50cec2b8eabaa834947f6a9220ed1924469a0202
Alex Grafe <alex.grafe@xanderia.org>
2016-11-08 16:10:38 +0100

    parse req

--- server.js
+++ server.js
@@ -1,6 +1,9 @@
 var express = require('express');
 var app = express();
 
+var bodyParser = require('body-parser');
+app.use(bodyParser.json()); // support json encoded bodies
+app.use(bodyParser.urlencoded({ extended: true })); // support encoded bodies
 
 
 app.get('/', function (req, res) {
@@ -10,7 +13,18 @@ app.get('/', function (req, res) {
 
 app.post('*', function (req, res) {
    console.log("Got a POST request for the homepage", req);
-   res.send(JSON.stringify(req));
+   res.send(JSON.stringify
+       (
+           {
+               "protocol":      req.protocol,
+               "hostname":      req.hostname,
+               "originalUrl":   req.originalUrl,
+               "path":          req.path,
+               "query":         req.query,
+               "headers":       req.headers,
+               "body":          req.body
+           }
+       ));
 })
 
 
@@ -20,8 +34,5 @@ app.post('*', function (req, res) {
 
 
 var server = app.listen(process.env.PORT || 8080, function () {
-   var host = server.address().address
-   var port = server.address().port
-
-   console.log("Example app listening at ", host, port)
+   console.log("Example app listening at ", server.address().address, server.address().port)
 });
